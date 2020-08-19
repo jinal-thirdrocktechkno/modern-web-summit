d780858bd50e3d9abd481e1e582263865536e764
jinal-thirdrocktechkno <jinal@thirdrocktechkno.com>
2020-08-19 16:36:10 +0530

    Input change

--- components/notify.tsx
+++ components/notify.tsx
@@ -8,8 +8,8 @@ const NotifyForm = (props: NotifyFormProps) => {
         {!showForm &&
             <button className={`custom-btn focus:outline-none ${buttonClass}`} type="button" onClick={() => setShowForm(!showForm)}>Notify me when tickets go on sale</button>
             || <div>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} />
-                <button onClick={() => {
+                <input className="py-2 px-6 mr-2 flex-grow border-solid border-2 text-xl border-black focus:outline-none" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <button className="bg-black text-white py-2 px-12 uppercase text-xl focus:outline-none" onClick={() => {
                     setShowForm(!showForm)
                     setEmail('')
                 }}>Submit</button>
