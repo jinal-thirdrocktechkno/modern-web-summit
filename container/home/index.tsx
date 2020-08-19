ed8d2b983c5c0256bef2d3c04adc122d61388ca5
jinal-thirdrocktechkno <jinal@thirdrocktechkno.com>
2020-08-19 10:42:15 +0530

    Icon change

--- container/home/index.tsx
+++ container/home/index.tsx
@@ -12,15 +12,15 @@ const Home = () => {
         <img className="inline p-2 ml-2 mt-1" src="/images/menu.png" alt="logo" />
         <div className="flex items-center mr-10">
           <p className="uppercase font-extrabold text-typographyLight text-opacity-25 tracking-widest">share</p>
-          <img className="inline p-2" src="/images/twitter.png" alt="logo" />
-          <img className="inline p-2" src="/images/linkedIn.png" alt="logo" />
-          <img className="inline p-2" src="/images/github.png" alt="logo" />
+          <img className="inline p-2" src="/images/Twitter.svg" alt="logo" />
+          <img className="inline p-2" src="/images/LinkedIn.svg" alt="logo" />
+          <img className="inline p-2" src="/images/github.svg" alt="logo" />
         </div>
       </div>
 
 
       <div className='landing-wrap flex'>
-        <img className="inline self-start -mt-10 ml-8" src="/images/left.png" alt="logo" />
+        <img className="inline self-start -mt-10 ml-8" src="/images/left.svg" alt="logo" />
         <div className='w-2/5 m-auto text-center -mt-10'>
           <Logo />
           <p className='text-typography font-medium text-base mb-10'>
@@ -28,7 +28,7 @@ const Home = () => {
           <NotifyForm />
           <p className='text-typography font-medium text-base mt-5'>{"Terms of Service • Privacy Policy • Code of Conduct"}</p>
         </div>
-        <img className="inline self-end mr-8 relative top-1/2 transform -translate-y-48" src="/images/right.png" alt="logo" />
+        <img className="inline self-end mr-8 relative top-1/2 transform -translate-y-48" src="/images/right.svg" alt="logo" />
       </div>
 
       <div className='w-2/5 m-10 text-center'>
